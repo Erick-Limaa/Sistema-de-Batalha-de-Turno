b71ec6a860d2e1e6a88ffc8e9653a4db6bb2d9f2
Erick-Limaa <erickbflima@gmail.com>
2025-04-02 15:01:44 -0300

    Adicionar arquivos de projeto.

---
+++ Sistema de Batalha de Turno/Program.cs
@@ -0,0 +1,41 @@
+﻿using Sistema_de_Batalha_de_Turno;
+
+Unit player = new Unit(100, 20, 12, "Jogador");
+Unit enemy = new Unit(75, 10, 7, "Mago do Mal");
+Random random = new Random();
+
+while (!player.isDead && !enemy.isDead)
+{
+    Console.WriteLine(player.UnitName + " HP = " + player.HP + ". " + enemy.UnitName + " HP = " + enemy.HP);
+    Console.WriteLine("Turno do jogador! O que você irá fazer? Ataque com 'a' e se cure com 'h'");
+    string choice = Console.ReadLine().ToLower();
+
+    if (choice == "a")
+    {
+        player.Attack(enemy);
+    }
+    else if (choice == "h")
+    {
+        player.Heal();
+    }
+    else
+    {
+        Console.WriteLine("Digite uma tecla válida!");
+    }
+
+    if (player.isDead || enemy.isDead) break;
+
+    Console.WriteLine(player.UnitName + " HP = " + player.HP + ". " + enemy.UnitName + " HP = " + enemy.HP);
+
+    Console.WriteLine("Turno do inimigo!");
+
+    int rand = random.Next(0, 2);
+
+    if (rand == 0)
+        enemy.Attack(player);
+    else
+    {
+        enemy.Heal();
+    }
+}
+
---
+++ Sistema de Batalha de Turno/Unit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Sistema_de_Batalha_de_Turno
+{
+    internal class Unit
+    {
+        private int currentHP;
+        private int maxHP;
+        private int attack;
+        private int healPower;
+        private string Unitname;
+        private Random random;
+
+        public int HP { get { return currentHP; } }
+        public String UnitName { get { return Unitname; } }
+
+        public bool isDead { get { return currentHP <= 0; } }
+
+        public Unit(int maxHP, int attack, int heal, String Unitname)
+        {
+            this.maxHP = maxHP;
+            this.currentHP = maxHP;
+            this.attack = attack;
+            this.healPower = heal;
+            this.Unitname = Unitname;
+            this.random = new Random();
+        }
+
+        public void Attack(Unit userToAttack)
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int randDamage = (int)(attack * rng);
+            userToAttack.Takendamage(randDamage);
+            Console.WriteLine(Unitname + " ataca " + userToAttack.Unitname + " e causa " + randDamage + " de dano!");
+        }
+
+        public void Takendamage(int damage)
+        {
+            currentHP -= damage;
+
+            if(isDead)
+                Console.WriteLine(UnitName + " foi derrotado!");
+        }
+
+        public void Heal()
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int heal = (int)(rng * healPower);
+            currentHP = heal = currentHP > maxHP ? maxHP : currentHP + heal;
+            Console.WriteLine(UnitName + " cura " + heal);
+        }
+    }
+}
